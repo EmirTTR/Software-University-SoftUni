ba6202e2bdf5b41384497016a971516a22275947
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-04 09:48:22 +0300

    Foreign languages

---
+++ foreignLanguages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            switch (name)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Argentina":
+                case "Spain":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}

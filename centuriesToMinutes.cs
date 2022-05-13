f0a97734c7eb80ea2127c157e614ac0483eeb086
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-13 19:17:50 +0300

    Centuries to minutes

---
+++ centuriesToMinutes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal centurees = decimal.Parse(Console.ReadLine());
+            decimal years = centurees * 100;
+            decimal days = (int)(years * 365.2422m);
+            decimal hours = (days * 24);
+            decimal minutes = hours * 60;
+
+            Console.WriteLine($"{centurees} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}

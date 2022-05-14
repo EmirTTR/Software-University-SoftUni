0b72aac7a2125fb05bf248cd91162529a3d6f2d5
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-14 18:27:06 +0300

    Chars to string

---
+++ charsToString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char seccond = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            Console.Write($"{first}{seccond}{third}");
+        }
+    }
+}

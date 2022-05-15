01d5c4e0b32145bd3653d1cfc87fa2d4e039eaa7
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-15 11:06:23 +0300

    Refactor volume of pyramid

---
+++ refactorVolumeOfPyramid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length, width, height;
+            length = double.Parse(Console.ReadLine());
+            width = double.Parse(Console.ReadLine());
+            height = double.Parse(Console.ReadLine());
+            double pyramid = (length * width * height)/3;
+
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+            Console.Write($"Pyramid Volume: {pyramid:f2}");
+        }
+    }
+}

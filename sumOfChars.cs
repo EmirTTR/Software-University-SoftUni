cf88789855bca8627d263c53b46d9453dd68a8ac
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-17 14:27:11 +0300

    Sum of Chars

---
+++ sumOfChars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCahars = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= numCahars; i++)
+            {
+                char otherChars = char.Parse(Console.ReadLine());
+                sum += otherChars;
+
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+
+        }
+    }
+}

fd5a22d663aaffa7603e2dc5c711cb084e98a7c6
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-20 10:49:17 +0300

    Even and Odd Subtraction

---
+++ evenAndOddSubstraction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]%2==0)
+                {
+                    evenSum += num[i];
+
+                }
+                if (num[i]%2!=0)
+                {
+                    oddSum += num[i];
+                }
+                sum = evenSum - oddSum;
+
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

9b89039428d25eb707f07419a55891c8ce775731
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-27 11:54:17 +0300

    Repeat String

---
+++ repeatingString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            repeatWord(word,num);
+        }
+
+        static void repeatWord(string word,int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}

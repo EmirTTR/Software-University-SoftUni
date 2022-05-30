3e89dc144722cca556e228f99409605b466bf086
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-30 12:24:24 +0300

    Middle Characters

---
+++ middleCharacters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+           string result=GetMidChar(word);
+           Console.Write(result);
+
+        }
+
+        static string GetMidChar(string word)
+        {
+            int lenght = word.Length;
+            string result=String.Empty;
+            if (lenght % 2 == 1)
+            {
+                result = word[word.Length / 2].ToString();
+            }
+            else
+            {
+                result = word[word.Length / 2 - 1].ToString() + word[word.Length / 2].ToString();
+            }
+
+            return result;
+        }
+    }
+}

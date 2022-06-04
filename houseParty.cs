d497c6bf8456239f7a18d689197d5e63458a7352
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-04 12:47:45 +0300

    House Party

---
+++ houseParty.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPeople = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < numPeople; i++)
+            {
+                List<string> names = Console.ReadLine().Split(" ").ToList();
+
+                  if (names[2]=="going!")
+                  {
+                      if (!guests.Contains(names[0]))
+                      {
+                          guests.Add(names[0]);
+                      }
+                      else
+                      {
+                          Console.WriteLine($"{names[0]} is already in the list!");
+                      }
+                  }
+                  else if (names[2]=="not")
+                  {
+                      if (guests.Contains(names[0]))
+                      {
+                          guests.Remove(names[0]);
+                      }
+                      else
+                      {
+                          Console.WriteLine($"{names[0]} is not in the list!");
+                      }
+                  }
+
+                
+            }
+
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine(guests[i]);
+            }
+        }
+    }
+}

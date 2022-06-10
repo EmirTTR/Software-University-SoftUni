c0574d384fbadc8d11d434013d0621de902aeb68
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-10 11:18:21 +0300

    Word Filter

---
+++ wordFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            words = words.Where(w => w.Length % 2 == 0).ToArray();
+            Console.WriteLine(string.Join("\n", words));
+        }
+    }
+}

b557bbdb2d92e30366c32b6bf18ad86060c1d532
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-13 12:03:16 +0300

    Substring

---
+++ substring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string remove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int indexOF = text.IndexOf(remove);
+
+            while (indexOF>=0)
+            {
+                text = text.Remove(indexOF, remove.Length);
+                indexOF = text.IndexOf(remove);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

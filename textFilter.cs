a5ad8fd04fe14050082ddac9b38c60aac0c0ec81
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-14 11:51:39 +0300

    Text Filter

---
+++ textFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] censoredWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var item in censoredWords)
+            { 
+              text = text.Replace(item, new string('*', item.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

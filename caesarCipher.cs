b51379fff9b5063dab0f9b66175d01e57ae16b9a
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-15 11:06:07 +0300

    Caesar Cipher

---
+++ caesarCipher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            int positionChar = 3;
+            StringBuilder complete = new StringBuilder();
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    char chart = words[j];
+                    int sumChart = chart;
+                    int fulChar = sumChart + positionChar;
+                    char megaChar = (char)fulChar;
+                    complete.Append(megaChar);
+                    
+                }
+            
+            Console.WriteLine(string.Join("",complete));
+        }
+    }
+}

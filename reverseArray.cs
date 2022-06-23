7b96692c6804c60a60e9633e74fa9658885b016a
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-23 13:20:09 +0300

    Reverse Array

---
+++ reverseArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] rev = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                rev[i] = arr[arr.Length - 1 - i];
+            }
+
+            for (int i = 0; i < rev.Length; i++)
+            {
+                Console.Write(rev[i] + " ");
+            }
+        }
+    }
+}

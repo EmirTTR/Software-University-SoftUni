3cfedf137a8de36cf9e164a213df6e49723392c5
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-06-30 11:51:36 +0300

    Division

---
+++ Programming Fund Homework/01.Basic Syntax, Conditional Statements and Loops/division.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02._Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int twoNum = 2;
+            int treeNum = 3;
+            int sixNum = 6;
+            int sevenNum = 7;
+            int tenNum = 10;
+
+            
+           
+            if (number%10==0)
+            {
+                Console.WriteLine($"The number is divisible by {tenNum}");
+            }
+            else if (number%7==0)
+            {
+                Console.WriteLine($"The number is divisible by {sevenNum}");
+
+            }
+            else if (number%6==0)
+            {
+                Console.WriteLine($"The number is divisible by {sixNum}");
+
+            }
+            else if (number%3==0)
+            {
+                Console.WriteLine($"The number is divisible by {treeNum}");
+
+            }
+            else if (number%2==0)
+            {
+                Console.WriteLine($"The number is divisible by {twoNum}");
+
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}

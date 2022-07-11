2bc944f4b877048f6a2ae67513e8cba31749cf4d
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-11 09:27:21 +0300

    Elevator

---
+++ Programming Fund Homework/02.Data Types and Variables/elevator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPeople = int.Parse(Console.ReadLine());
+            int elevatorCapaciy = int.Parse(Console.ReadLine());
+
+            int coureses = (int)Math.Ceiling((double)numPeople / elevatorCapaciy);
+            
+
+            
+            
+            
+                Console.WriteLine($"{coureses}");
+            
+
+        }
+
+
+    }
+}

3a223e987da23af15d83b6876f4bb46a295573c8
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-12 09:19:41 +0300

    Triple Of latin

---
+++ Programming Fund Homework/02.Data Types and Variables/tripleOfLatinLetters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number ; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    for (int k = 0; k < number; k++)
+                    {
+                        char first = (char)('a' + i);
+                        char seccond=(char)('a' + j);
+                        char third=(char)('a' + k);
+
+
+                        Console.WriteLine($"{first}{seccond}{third}");
+                    }
+                }
+            }
+        }
+    }
+}

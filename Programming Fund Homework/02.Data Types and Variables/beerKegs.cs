9c4f5c3663f24db850cd9c43378dfe4cbaef090f
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-13 11:28:05 +0300

    Beer Kegs

---
+++ Programming Fund Homework/02.Data Types and Variables/beerKegs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberKegs = int.Parse(Console.ReadLine());
+            string biggestKeg = string.Empty;
+            double maxNum = double.MinValue;
+            
+
+            for (int i = 0; i < numberKegs; i++)
+            {
+                string kegName = Console.ReadLine();
+                float kegWidth = float.Parse(Console.ReadLine());
+                float kegHeight = float.Parse(Console.ReadLine());
+                
+                double currentVolume = Math.PI * kegWidth * kegWidth * kegHeight;
+
+                if (currentVolume>maxNum)
+                {
+
+                    maxNum=currentVolume;
+                    biggestKeg = kegName;
+                }
+
+
+                
+            }
+                    Console.WriteLine(biggestKeg);
+        }
+    }
+}

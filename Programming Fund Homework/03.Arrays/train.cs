c1873bab327b783f23f693ebff87ef2a9ad76653
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-16 11:16:54 +0300

    Train

---
+++ Programming Fund Homework/03.Arrays/train.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagoons = int.Parse(Console.ReadLine());
+            int[] peoples = new int[wagoons];
+            int sum = 0;
+
+            for (int i = 0; i < wagoons; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                peoples[i] = people;
+               
+                
+
+            }
+            for (int i = 0; i < peoples.Length; i++)
+            {
+                sum += peoples[i];
+                Console.Write(peoples[i] + " ");
+
+            }
+            Console.WriteLine(" ");
+            Console.Write(sum);
+                  
+        }
+    }
+}

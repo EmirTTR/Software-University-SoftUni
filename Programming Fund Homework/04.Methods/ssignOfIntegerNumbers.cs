ec23551c955cecf9b301ca1c1d1ae8579f658cd3
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-21 11:16:29 +0300

    Sign of integer numbers

---
+++ Programming Fund Homework/04.Methods/ssignOfIntegerNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            numType(num);
+
+        }
+
+        static void numType (int number)
+        {
+            if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number==0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}

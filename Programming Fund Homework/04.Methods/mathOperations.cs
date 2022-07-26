08b7380c81fcae850809a8b8f2699e7fe9987f2c
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-26 09:41:30 +0300

    Math Operations

---
+++ Programming Fund Homework/04.Methods/mathOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            char operatorOfMat = char.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (operatorOfMat=='+')
+            {
+                 AddOperator(num1, num2);
+            }
+            else if (operatorOfMat=='-')
+            {
+                 SubtractOperator(num1, num2);
+            }
+            else if (operatorOfMat=='*')
+            {
+                MultiplyOfMat(num1, num2);
+            }
+            else if (operatorOfMat=='/')
+            {
+                DivideOfMat(num1, num2);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private static void DivideOfMat(int num1, int num2)
+        {
+            int sum = num1 / num2;
+            Console.WriteLine(sum);
+        }
+
+        static void MultiplyOfMat(int num1, int num2)
+         {
+             int sum = num1 * num2;
+             Console.WriteLine(sum);
+         }
+
+        static void SubtractOperator(int num1, int num2)
+        {
+            int sum = num1 - num2; 
+            Console.WriteLine(sum);
+        }
+
+         static void AddOperator(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}

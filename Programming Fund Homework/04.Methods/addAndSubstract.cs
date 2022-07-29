81446ef2d7081fc07b1aefefb49022b54970ffa7
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-07-29 08:40:23 +0300

    Add and Subtract

---
+++ Programming Fund Homework/04.Methods/addAndSubstract.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int result=SubstractSum(num1, num2,num3);
+            Console.WriteLine(result);
+
+        }
+
+         static int SubstractSum(int num1, int num2,int num3)
+         {
+            int result = AddNums(num1, num2);
+            result -= num3;
+            return result;
+
+         }
+
+        static int AddNums(int num1, int num2)
+        {
+            int result = 0;
+            result = num1 + num2;
+            return result;
+        }
+    }
+}

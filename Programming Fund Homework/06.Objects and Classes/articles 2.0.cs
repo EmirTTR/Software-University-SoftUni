2276b62440f1c97bd212abfdf2ec1df56bfb46d2
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-08-14 08:30:59 +0300

    Articles 2.0

---
+++ Programming Fund Homework/06.Objects and Classes/articles 2.0.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+            string[] input;
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split(", ");
+                Article article = new Article (input[0], input[1], input[2]);
+              
+                articles.Add(article);
+            }
+            string[] command = Console.ReadLine().Split();
+
+           if (command[0] == "title")
+            {
+                foreach (var item in articles.OrderBy(x => x.Title))
+                {
+                }
+                    Console.WriteLine(string.Join(Environment.NewLine, articles));
+            }
+            else if (command[0] == "content")
+            {
+                foreach (var item in articles.OrderBy(x=>x.Content))
+                {
+                }
+                    Console.WriteLine(string.Join(Environment.NewLine, articles));
+            }
+            else if (command[0] == "author")
+            {
+                foreach (var item in articles.OrderBy(x => x.Author))
+                {
+                }
+                    Console.WriteLine(string.Join(Environment.NewLine, articles));
+            }
+
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}

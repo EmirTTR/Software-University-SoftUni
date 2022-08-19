651226b53fac8d57f4bf98ae6708333b42101f72
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-08-19 08:46:45 +0300

    Word Synonims

---
+++ Programming Fund Homework/07.Associative Arrays/wordSynonims.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonymDict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+
+                if (!synonymDict.ContainsKey(word))
+                {
+                    synonymDict.Add(word, new List<string>());
+                }
+               
+                synonymDict[word].Add(synonyms);
+            }
+            foreach (var item in synonymDict)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ",item.Value)}");
+            }
+        }
+    }
+}

a55331e935ae8496d462b027ed91ed5991ae6f21
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-08-26 09:11:34 +0300

    Substring

--- Programming Fund Homework/08.Text Processing/substring.cs
+++ Programming Fund Homework/08.Text Processing/substring.cs
@@ -1,6 +1,22 @@
-﻿namespace _02._Repeat_Strings
+﻿using System;
+
+namespace _03._Substring
 {
-    internal class substring
+    class Program
     {
+        static void Main(string[] args)
+        {
+            string remove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int indexOF = text.IndexOf(remove);
+
+            while (indexOF>=0)
+            {
+                text = text.Remove(indexOF, remove.Length);
+                indexOF = text.IndexOf(remove);
+            }
+            Console.WriteLine(text);
+        }
     }
 }

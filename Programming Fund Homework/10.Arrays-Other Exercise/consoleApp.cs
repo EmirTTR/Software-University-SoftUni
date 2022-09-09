da2e137a75e47b4170a8c14493b2ce07b6281c21
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-09-09 09:22:15 +0300

    Console App

---
+++ Programming Fund Homework/10.Arrays-Other Exercise/consoleApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] array = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The Even elements are:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.Write($"{array[i]} ");
+                    
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"The Odd elements are:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (array[i] % 2 != 0)
+                {
+                    Console.Write($"{array[i]} ");
+
+                }
+            }
+            
+        }
+    }
+}

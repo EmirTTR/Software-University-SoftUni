751b865a3207a72e8efb695eeb8bd5a4a0c93527
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-09-12 10:21:50 +0300

    Frequency of Elements

---
+++ Programming Fund Homework/10.Arrays-Other Exercise/frequencyOFElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FriquencyOfElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] elements = new int[num];
+            int count = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                elements[i] = int.Parse(Console.ReadLine());
+
+                if ()
+                {
+                    
+                }
+            }
+        }
+    }
+}

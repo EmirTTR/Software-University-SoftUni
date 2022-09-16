40b8c35ff05ccde85b444c9b5400427b1cff3a89
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-09-16 09:45:16 +0300

    Search Even Numbers

---
+++ Programming Fund Homework/10.Arrays-Other Exercise/searchEvenNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+           
+           
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("The even numbers are: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]%2==0)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+}

e0881acdbc7bb6a006db9e7db04eb7a003dad42f
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-09-17 10:09:24 +0300

    Sum of Array

---
+++ Programming Fund Homework/10.Arrays-Other Exercise/sumOfArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] arrayOfNums = new int[num];
+            int sumOfNums = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                arrayOfNums[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < arrayOfNums.Length; i++)
+            {
+                sumOfNums += arrayOfNums[i];
+            }
+            Console.WriteLine(sumOfNums);
+        }
+    }
+}

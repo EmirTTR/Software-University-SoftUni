cb71c385b555d6b8d459c4d559fced3d10ef5f45
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-09-24 11:03:07 +0300

    Stack sum

--- C# Advanced/01.Stacks and Queues/Program.cs
+++ C# Advanced/01.Stacks and Queues/Program.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _1._Reverse_Strings
+namespace _2._Stack_Sum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string word = Console.ReadLine();
-            Stack<char> symbools = new Stack<char>();
-            foreach (var characters in word)
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> numbersStack = new Stack<int>();
+            foreach (var number in numbers)
             {
-                symbools.Push(characters);
-            }
-            while (symbools.Count>0)
-            {
-                Console.Write(symbools.Pop());
+                numbersStack.Push(number);
             }
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(" ");
+                string command = splitInput[0].ToLower();
+                if (command=="end")
+                {
+                    break;
+                }
+                else if (command=="add")
+                {
+                    int firstNum = int.Parse(splitInput[1]);
+                    int secondNum = int.Parse(splitInput[2]);
+                    numbersStack.Push(firstNum);
+                    numbersStack.Push(secondNum);
+                }
+                else if (command=="remove")
+                {
+                    int numbersToRemove = int.Parse(splitInput[1]);
+                    if (numbersToRemove<numbersStack.Count)
+                    {
+                        for (int i = 0; i < numbersToRemove; i++)
+                        {
+                            numbersStack.Pop();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {numbersStack.Sum()}");
         }
     }
 }

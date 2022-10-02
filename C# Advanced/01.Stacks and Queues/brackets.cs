56d76337ca9eac4fe6a17fc7edeaa2b09619ac66
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-02 10:02:48 +0300

    Matching brackets

---
+++ C# Advanced/01.Stacks and Queues/brackets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> inputIndex = new Stack<int>();
+            string ready = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]=='(')
+                {
+                    int open = i;
+                    inputIndex.Push(open);
+                    
+                }
+                if (input[i]==')')
+                {
+                    int open = inputIndex.Pop();
+                    int close = i;
+                    ready = input.Substring(open,close - open + 1);
+                    Console.WriteLine(ready);
+                }
+
+            }
+        }
+    }
+}

02868ed8f2727a95bdde7aa0982c3f6f9b2f49d7
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-03 10:25:16 +0300

    Hot Potato

---
+++ C# Advanced/01.Stacks and Queues/hotPotato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int numForElimination = int.Parse(Console.ReadLine());
+            Queue<string> pleyarsQueue = new Queue<string>();
+            foreach (var kid in players)
+            {
+                pleyarsQueue.Enqueue(kid);
+            }
+            while (pleyarsQueue.Count > 1)
+            {
+                for (int i = 0; i < numForElimination - 1; i++)
+                {
+
+                    string nextKid = pleyarsQueue.Dequeue();
+                    pleyarsQueue.Enqueue(nextKid);
+                }
+                Console.WriteLine($"Removed {pleyarsQueue.Dequeue()}");
+
+            }
+            Console.WriteLine($"Last is {string.Join("",pleyarsQueue)}");
+        }
+    }
+}

7ea104f64691379765bf21a1de5f3ac4d23669d1
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-04 09:58:53 +0300

    Basic queue operations

---
+++ C# Advanced/01.Stacks and Queues/basicQueueOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int numberOfElements = int.Parse(commands[0].ToString());
+            int elementsToPop = int.Parse(commands[1].ToString());
+            int elementToFind = int.Parse(commands[2].ToString());
+            int minElement = int.MaxValue;
+            int elementFromQueue = 0;
+
+            Queue<int> elementsStack = new Queue<int>();
+            int[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            if (elements.Length == numberOfElements)
+            {
+                foreach (var item in elements)
+                {
+                    elementsStack.Enqueue(item);
+                }
+                while (elementsToPop > 0)
+                {
+                    elementsToPop--;
+                    elementsStack.Dequeue();
+                }
+                if (elementsStack.Contains(elementToFind))
+                {
+                    Console.WriteLine("true");
+                }
+                else if (!elementsStack.Contains(elementToFind))
+                {
+                    if (elementsStack.Count < 1)
+                    {
+                        Console.WriteLine("0");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < elements.Length; i++)
+                        {
+                            elementFromQueue = int.Parse(elements[i].ToString());
+                            if (elementFromQueue<minElement)
+                            {
+                               minElement=elementFromQueue;
+                            }
+                        }
+                        Console.WriteLine(minElement);
+                    }
+                }
+            }
+        }
+    }
+}

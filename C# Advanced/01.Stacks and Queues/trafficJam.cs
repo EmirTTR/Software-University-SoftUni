fa1eb8ce24e1f4ce130cac79c11d23a992a9590d
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-04 09:57:21 +0300

    Traffic jam

---
+++ C# Advanced/01.Stacks and Queues/trafficJam.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCarCanPast = int.Parse(Console.ReadLine());
+            Queue<string> carsQueue = new Queue<string>();
+            int carNum = 0;
+            int passedCars = 0;
+
+            while (true)
+            {
+                string car = Console.ReadLine();
+                if (car!="green")
+                {
+                    carsQueue.Enqueue(car);
+                    carNum++;
+                }
+                if (car=="end")
+                {
+                    break;
+                }
+                else if (car=="green")
+                {
+                    int carPass = numberCarCanPast;
+                    
+                     while (carPass>0)
+                     {
+
+                        if (carsQueue.Count>0)
+                        {
+                             carPass--;
+                             Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                     }
+                }
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}

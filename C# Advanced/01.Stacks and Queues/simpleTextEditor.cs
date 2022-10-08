7a2da135e922a884038e097b08ed4aad917c8f3f
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-08 10:50:36 +0300

    Simple text editor

---
+++ C# Advanced/01.Stacks and Queues/simpleTextEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nCommands = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> textStack = new Stack<string>();
+
+            textStack.Push(text.ToString());
+            for (int i = 0; i < nCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string command = input[0];
+
+                switch (command)
+                {
+                    case "1":
+                        text.Append(input[1]);
+                        textStack.Push(text.ToString());
+                        break;
+                    case "2":
+                        int index = int.Parse(input[1]);
+                        text.Remove(text.Length - index, index);
+                        textStack.Push(text.ToString());
+                        break;
+                    case "3":
+                        int place = int.Parse(input[1]);
+                        Console.WriteLine(text[place-1]);
+                        break;
+                    case "4":
+                        textStack.Pop();
+                        text = new StringBuilder();
+                        text.Append(textStack.Peek());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+           
+        }
+    }
+}

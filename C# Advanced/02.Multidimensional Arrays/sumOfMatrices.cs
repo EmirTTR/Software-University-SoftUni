aa2d3d2bb336d28641cb6e506f6593b51048eaca
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-08 10:51:14 +0300

    Sum of Matrices

---
+++ C# Advanced/02.Multidimensional Arrays/sumOfMatrices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] needNums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int row = needNums[0];
+            int col = needNums[1];
+            int[,] table =  new int[row,col];
+            int sum = 0;
+            
+
+            for (int rows = 0; rows <table.GetLongLength(0); rows++)
+            {
+                    string colsLine = Console.ReadLine();
+                    string[] splitColsLine = colsLine.Split(", ");
+                for (int cols = 0; cols < table.GetLongLength(1); cols++)
+                {
+                    table[rows, cols] = int.Parse(splitColsLine[cols]);
+                    
+                }
+                
+            }
+            foreach (var num in table)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(table.GetLongLength(0));
+            Console.WriteLine(table.GetLongLength(1));
+            Console.WriteLine(sum);
+
+        }
+    }
+}

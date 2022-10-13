e188e2c44c7d5f631e9bc99a8518d2fb153fa2d6
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-13 10:29:35 +0300

    Diagonal Difference

---
+++ C# Advanced/02.Multidimensional Arrays/Diagonal Difference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] matrix = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            }
+            long sumLeftDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                sumLeftDiagonal += matrix[row][row];
+            }
+
+            long sumRightDiagonal = 0;
+            for (int row = 0, col = n - 1; row < n; row++, col--)
+            {
+                sumRightDiagonal += matrix[row][col];
+            }
+            long finalSum = sumLeftDiagonal - sumRightDiagonal;
+            Console.WriteLine(Math.Abs(finalSum));
+        }
+    }
+}

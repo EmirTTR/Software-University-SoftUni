18d753f966670dd53f17a19cc3ac9bb9e07c36f6
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-18 12:43:12 +0300

    Average Student Grades

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Average Student Grades.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentsDict = new Dictionary<string, List<decimal>>();
+            decimal averageGrade = 0;
+            decimal sumGrade = 0;
+            
+            
+
+            for (int i = 0; i < numStudents; i++)
+            {
+                string input = Console.ReadLine();
+                string[] inputParts = input.Split(" ");
+                string name = inputParts[0];
+                decimal grade = decimal.Parse(inputParts[1]);
+                if (!studentsDict.ContainsKey(name))
+                {
+                    studentsDict.Add(name, new List<decimal>());
+                }
+                 studentsDict[name].Add(grade);
+            }
+
+            foreach (var item in studentsDict)
+            {
+                Console.Write($"{item.Key} -> ");
+                foreach (var grade in item.Value)
+                {
+                    decimal num = decimal.Parse(grade.ToString());
+                    sumGrade += num;
+                    Console.Write($"{grade:f2} ");
+                }
+                averageGrade = sumGrade / item.Value.Count;
+                sumGrade = 0;
+                Console.Write($"(avg: {averageGrade:f2})");
+                Console.WriteLine();
+            }
+        }
+    }
+}

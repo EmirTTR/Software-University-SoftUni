814afa174ac7df0da839d1b2c010c44aa7bfd583
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-19 10:22:02 +0300

    Largest 3 Numbers

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Largest 3 Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            List<int> numList = new List<int>();
+            int count = 3;
+            foreach (var item in numbers)
+            {
+                numList.Add(item);
+            }
+            numList.Sort();
+            numList.Reverse();
+
+            for (int i = 0; i < numList.Count; i++)
+            {
+                if (i<count)
+                {
+                    Console.Write(numList[i] + " ");
+                }
+            }
+
+          
+        }
+    }
+}

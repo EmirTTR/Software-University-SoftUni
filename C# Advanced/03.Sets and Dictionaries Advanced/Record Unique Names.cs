d390bf16acb014de83c3f04e8dd437c4a46506e3
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-24 11:58:20 +0300

    Record Unique Names

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Record Unique Names.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+            int numNames = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numNames; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+            foreach (var item in uniqueNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

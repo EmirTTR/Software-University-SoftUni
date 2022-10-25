2ee2829c570875e02b8bb71871a93c5969b9f4c2
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-25 11:49:24 +0300

    Unique Usernames

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Unique Usernames.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nTimes = int.Parse(Console.ReadLine());
+            HashSet<string> nameSet = new HashSet<string>();
+            Queue<string> nameQueue = new Queue<string>();
+
+
+            for (int i = 0; i < nTimes; i++)
+            {
+                string name = Console.ReadLine();
+                nameSet.Add(name);
+
+            }
+
+            foreach (var item in nameSet)
+            {
+                nameQueue.Enqueue(item);
+            }
+            foreach (var item in nameQueue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

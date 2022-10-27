b0c368759db5f540805dea00733d3d1b43ece6be
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-27 14:01:19 +0300

    Periodic Table

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Periodic Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nTimes = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueElemenst = new HashSet<string>();
+
+            for (int i = 0; i < nTimes; i++)
+            {
+                string[] elements = Console.ReadLine().Split(" ");
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    uniqueElemenst.Add(elements[j]);
+                }
+            }
+            foreach (var item in uniqueElemenst.OrderBy(x=>x))
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}

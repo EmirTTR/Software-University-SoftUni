4934c450fc456569df8051816583f3d172343ab7
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-10-29 11:37:21 +0300

    Even Times

---
+++ C# Advanced/03.Sets and Dictionaries Advanced/Even Times.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nTimes = int.Parse(Console.ReadLine());
+            List<int> uniqueNumbers = new List<int>();
+
+            for (int i = 0; i < nTimes; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                uniqueNumbers.Add(number);
+            }
+
+            for (int i = 0; i < uniqueNumbers.Count; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(uniqueNumbers[i]);
+                    return;
+                }
+            }
+        }
+    }
+}

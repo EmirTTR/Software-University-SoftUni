43fb0d598e255e899f75ad519b5d32f1d55432da
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-12-08 13:36:00 +0200

    ListOfPredicates.cs

---
+++ C# Advanced/05.Functional Programming/08.List Of Predicates/ListOfPredicates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class ListOfPredicates
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int[] toDivide = Console.ReadLine().Split(' ').Select(int.Parse).Distinct().ToArray();
+
+            Func<int, bool>[] predicates = toDivide.Select(div => (Func<int, bool>)(n => n % div == 0)).ToArray();
+
+            for (int j = 1; j <= range; j++)
+            {
+                bool isDivisable = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(j))
+                    {
+                        isDivisable = false;
+                        break;
+                    }
+                }
+
+                if (isDivisable)
+                {
+                    Console.Write("{0} ", j);
+                }
+            }
+        }
+    }
+}

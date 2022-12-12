efed0570334f06ca0426f289608335439cda9ccd
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-12-12 10:49:54 +0200

    Update matchFullName.cs

--- Programming Fund Homework/09.Regular Expressions/matchFullName.cs
+++ Programming Fund Homework/09.Regular Expressions/matchFullName.cs
@@ -9,7 +9,7 @@ namespace _01._Match_Full_Name
         {
             string input = Console.ReadLine();
             MatchCollection regex = Regex.Matches(input, @"\b[A-Z][a-z]+ [A-Z][a-z]+");
-
+///////////////
             foreach (Match item in regex)
             {
                 Console.Write(item + " ");

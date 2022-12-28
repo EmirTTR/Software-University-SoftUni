cb99abe9dc026664ddb1c453ab8f46e526732e77
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-12-28 10:40:57 -0800

    Update matchDates.cs

--- Programming Fund Homework/09.Regular Expressions/matchDates.cs
+++ Programming Fund Homework/09.Regular Expressions/matchDates.cs
@@ -13,7 +13,7 @@ namespace _03._Match_Dates
             string input = Console.ReadLine();
             Regex datePattern = new Regex(@"(?<day>[0-9]{2})(?<sep>\.?\-?\/?)(?<month>[A-Z]{1}[a-z]{2})\k<sep>(?<year>[0-9]{4})\b");
             MatchCollection matchingDates = datePattern.Matches(input);
-
+////////////////////
             foreach (Match item in matchingDates)
             {
                 Console.WriteLine($"Day: {item.Groups["day"].Value}, Month: {item.Groups["month"].Value}, Year: {item.Groups["year"].Value}");
